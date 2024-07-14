574dd88dc174102e6b5303ea01d4bf80da529be6
pierrot498 <pierre.etienne.horreau@gmail.com>
2024-07-14 05:46:39 +0200

    Init - ApeCoinFeeReduction

---
+++ ApeCoin/contracts/ApeFeeReducerHook.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {FullRange} from "./FullRange.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/src/types/PoolId.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "@uniswap/v4-core/src/types/BeforeSwapDelta.sol";
+import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
+import {CurrencyLibrary, Currency} from "@uniswap/v4-core/src/types/Currency.sol";
+import {CurrencySettler} from "@uniswap/v4-core/test/utils/CurrencySettler.sol";
+
+contract FeeReducerHook is FullRange {
+    IERC20 public feeToken;
+
+    constructor(IPoolManager _manager,address _feeToken) FullRange(_manager) {
+        feeToken = IERC20(_feeToken);
+    }
+
+    function beforeSwap(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata params,
+        bytes calldata data
+    ) external override returns (bytes4, BeforeSwapDelta, uint24) {
+        PoolId poolId = PoolIdLibrary.toId(key);
+
+        if (!poolInfo[poolId].hasAccruedFees) {
+            PoolInfo storage pool = poolInfo[poolId];
+            pool.hasAccruedFees = true;
+        }
+
+        uint24 fee = key.fee;
+        if (feeToken.balanceOf(sender) >= 100 ) {
+            fee = fee / 2;
+        }
+
+        return (IHooks.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, fee);
+    }
+}
